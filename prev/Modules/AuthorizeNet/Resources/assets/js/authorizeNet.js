cd772011667d7a51ef5bdc70e4da9af1cb66775a
Maksym <buckenmeyerteo@gmail.com>
2024-02-05 13:50:51 +0000

    updated: project setup

--- prev/Modules/AuthorizeNet/Resources/assets/js/authorizeNet.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-function numberOnly(id) {
-    var element = document.getElementById(id);
-    element.value = element.value.replace(/[^0-9]/gi, "");
-}
--- prev/Modules/Paytm/Resources/assets/js/paytm-redirect.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-$( "form" ).trigger( "submit");
